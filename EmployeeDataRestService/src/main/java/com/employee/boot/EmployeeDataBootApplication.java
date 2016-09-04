6f469f9b899fe0806cb65183d997d18997d2d173
sourav <souravgy09@gmail.com>
2016-09-04 13:11:29 +0530

    Employee Project Commit

---
+++ EmployeeDataRestService/src/main/java/com/employee/boot/EmployeeDataBootApplication.java
@@ -0,0 +1,15 @@
+package com.employee.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDataBootApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EmployeeDataBootApplication.class, args);
+
+	}
+
+}
